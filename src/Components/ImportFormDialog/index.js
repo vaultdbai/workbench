7b2e3a417674f01e807d2063e7fd50467b169827
Karanveer Sandhu <nostalgia.0145213@gmail.com>
2023-05-31 13:23:17 -0400

    Updated ImportFormDialog Added upload capability and showed uploaded file name on form

--- src/Components/ImportFormDialog/index.js
+++ src/Components/ImportFormDialog/index.js
@@ -10,6 +10,7 @@ import makeStyles from "@mui/styles/makeStyles";
 import Typography from "@mui/material/Typography";
 import PropTypes from "prop-types";
 import { DEFAULT_STRINGS, noop } from "utils/constants/common";
+import { useCallback, useState } from "react";
 
 const useStyles = makeStyles({
   input: {
@@ -21,17 +22,39 @@ const ImportFormDialog = (props) => {
 
   const { showDialog, handleCancelAction, handleSuccessAction } = props;
 
+  const [fileName, setFileName] = useState("");
+
+  // make sure to remove the file name if we close the window.
+  const closeDialog = useCallback(() => {
+    setFileName("")
+    handleCancelAction()
+  }, [setFileName])
+
+  const successfullyCloseDialog = useCallback(() => {
+    setFileName("")
+    handleSuccessAction()
+  }, [setFileName])
+
+  /**
+   * Function called when user opens a file in the import dialog
+   * @param {the event that triggers when a user opens a file in the import dialog} event 
+   */
+  const handleFileOpen = (event) => {
+    const file = event.target.files[0];
+    setFileName(file.name);
+  }
+
   const classes = useStyles();
   return (
     <Dialog
       open={showDialog}
-      onClose={handleCancelAction}
+      onClose={closeDialog}
       aria-labelledby="import-data-form-dialog-title"
     >
       {/* Title Section */}
       <DialogTitle
         id="import-data-form-dialog-title"
-        onClose={handleCancelAction}
+        onClose={closeDialog}
       >
         {DEFAULT_STRINGS.IMPORT_DATA_DIALOG_TITLE}
       </DialogTitle>
@@ -45,13 +68,14 @@ const ImportFormDialog = (props) => {
           justifyContent="space-between"
           alignItems="center"
         >
-          <Typography> Select File </Typography>
+          {fileName === "" ? <Typography> Select File </Typography> : <Typography>{fileName}</Typography>}
           <label htmlFor="file-upload">
             <input
               accept=".csv, .sql, .json, .xml"
               className={classes.input}
               id="file-upload"
               type="file"
+              onChange={handleFileOpen}
             />
             <Button variant="outlined" color="secondary" component={"span"}>
               {DEFAULT_STRINGS.BUTTON_OPEN_TEXT}
@@ -62,12 +86,12 @@ const ImportFormDialog = (props) => {
 
       {/* Dialog Action Buttons */}
       <DialogActions>
-        <Button onClick={handleCancelAction}>
+        <Button onClick={closeDialog}>
           {DEFAULT_STRINGS.BUTTON_CANCEL_TEXT}
         </Button>
         <Button
           variant="contained"
-          onClick={handleSuccessAction}
+          onClick={successfullyCloseDialog}
           color="secondary"
         >
           {DEFAULT_STRINGS.BUTTON_UPLOAD_TEXT}
