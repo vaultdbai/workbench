0db09ab4b8b5da4b3c78bf7f3947bc244d2384d5
Karanveer Sandhu <nostalgia.0145213@gmail.com>
2023-06-26 11:56:09 -0400

    Updated SideBar Fixed visual bug where components in Sidebar would overlap as components expand.

--- src/Components/SideBar/index.js
+++ src/Components/SideBar/index.js
@@ -12,10 +12,11 @@ import Proptypes from "prop-types";
 import Book from "@mui/icons-material/Book";
 import { getSyntaxMockData } from "utils/mockData";
 import { useState } from "react";
-import { Button, Collapse, Fab, TextField } from "@mui/material";
+import { Button, Collapse, Fab, Stack, TextField } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from '@mui/icons-material/Add';
 import Configuration from "Configration";
+import Scrollbar from 'react-scrollbar';
 
 // SideBar Styles
 const useStyles = makeStyles((theme) => ({
@@ -72,7 +73,7 @@ const SideBar = ({ showDrawer = false, items = [], catalogues = [], changeCatalo
     setAddCatalogueName(event.target.value);
   }
 
-  // Don't accept catalogue name if it already exists and if it includes a period
+  // Don't accept catalogue name if it already exists and if it includes any punctuation
   let catalogeNameError = false;
   let catalogNameHelperText = "";
   if (catalogues.includes(addCatalogueName)) {
@@ -94,6 +95,9 @@ const SideBar = ({ showDrawer = false, items = [], catalogues = [], changeCatalo
   } else if (addCatalogueName.length > 50) {
     catalogNameHelperText = "Catalogue name is too long!";
     catalogeNameError = true;
+  } else if (addCatalogueName === "table") {
+    catalogNameHelperText = "Catalogue name cannot be that"
+    catalogeNameError = true;
   } else {
     catalogNameHelperText = "";
     catalogeNameError = false;
@@ -110,82 +114,87 @@ const SideBar = ({ showDrawer = false, items = [], catalogues = [], changeCatalo
       }}
       open={showDrawer}
     >
-      {/* Large button labelled Catalogues that opens the list of catalogues */}
-      <Button onClick={toggleCatalogueDropdown}>
-        <Box p={1}>
-          <Typography variant="h6">Catalogues</Typography>
-          <ExpandMoreIcon />
-        </Box>
-      </Button>
-      {/* Contains the list of catalogues */}
-      <Collapse in={isOpen}>
-        {catalogues.length === 0 ? (
-          <EmptyState
-            title={DEFAULT_STRINGS.NO_CATALOGUE_EXIST}
-            titleVariant="h6"
-            subtitle={DEFAULT_STRINGS.CREATE_NEW_CATALOGUE_MESSAGE} />
-        ) : (
-          <List>
-            {catalogues.map((catalogue, index) => (
-              <DatabaseSidebarListItem
-                selected={selectedItem === catalogue}
-                key={`${catalogue}-${index}`}
-                item={catalogue}
-                onItemClick={handleItemClick}
-                databaseName={catalogue} />
-            ))}
-          </List>
-        )}
-        {/* The text field where a user can add a new catalogue name */}
-        <Box display="flex" alignItems="center">
-          <TextField
-            label="Add catalogue"
-            value={addCatalogueName}
-            onChange={handleInputChange}
-            error={catalogeNameError}
-            helperText={catalogNameHelperText}
-          />
-          <Fab
-            disabled={catalogeNameError || addCatalogueName.length <= 0}
-            size="small"
-            sx={{ padding: "20px" }}>
-            <AddIcon></AddIcon>
-          </Fab>
-        </Box>
-      </Collapse>
-      {/* Large button that opens the list of the current catalogue's tables */}
-      <Button onClick={toggleTableDropdown}>
-        <Box p={1}>
-          <Typography variant="h6">Tables</Typography>
-          <ExpandMoreIcon />
-        </Box>
-      </Button>
-      {/* Contains the list of tables of the current catalogue */}
-      <Collapse in={isTablesOpen}>
-        {items.length === 0 ? (
-          <EmptyState
-            title={DEFAULT_STRINGS.NO_TABLES_EXIST}
-            titleVariant="h6"
-            subtitle={DEFAULT_STRINGS.IMPORT_NEW_DATA_MESSAGE}
-          />
-        ) : (
-          <List>
-            {items.map((item, index) => (
-              <SidebarListItem
-                key={`${item.tableName}-${index}-table-metadata`}
-                listItem={item}
+      <Scrollbar style={{ height: "100%" }}>
+        {/* Large button labelled Catalogues that opens the list of catalogues */}
+        <Stack direction='column' spacing={1}>
+          <Button onClick={toggleCatalogueDropdown} sx={{ width: "100%" }}>
+            <Box p={1}>
+              <Typography variant="h6">Catalogues</Typography>
+              <ExpandMoreIcon />
+            </Box>
+          </Button>
+          {/* Contains the list of catalogues */}
+          <Collapse in={isOpen} unmountOnExit>
+            {catalogues.length === 0 ? (
+              <EmptyState
+                title={DEFAULT_STRINGS.NO_CATALOGUE_EXIST}
+                titleVariant="h6"
+                subtitle={DEFAULT_STRINGS.CREATE_NEW_CATALOGUE_MESSAGE} />
+            ) : (
+              <List>
+                {catalogues.map((catalogue, index) => (
+                  <DatabaseSidebarListItem
+                    selected={selectedItem === catalogue}
+                    key={`${catalogue}-${index}`}
+                    item={catalogue}
+                    onItemClick={handleItemClick}
+                    databaseName={catalogue} />
+                ))}
+              </List>
+            )}
+            {/* The text field where a user can add a new catalogue name */}
+            <Box display="flex" alignItems="center">
+              <TextField
+                label="Add catalogue"
+                value={addCatalogueName}
+                onChange={handleInputChange}
+                error={catalogeNameError}
+                helperText={catalogNameHelperText}
               />
-            ))}
-          </List>
-        )}
-      </Collapse>
-      <Box py={2}>
-        <SidebarListItem
-          listItem={getSyntaxMockData()}
-          icon={<Book />}
-          subtitle={"Queries"}
-        />
-      </Box>
+              <Fab
+                disabled={catalogeNameError || addCatalogueName.length <= 0}
+                size="small"
+                sx={{ padding: "20px" }}>
+                <AddIcon></AddIcon>
+              </Fab>
+            </Box>
+          </Collapse>
+          {/* Large button that opens the list of the current catalogue's tables */}
+          <Button onClick={toggleTableDropdown} sx={{ width: "100%" }}>
+            <Box p={1}>
+              <Typography variant="h6">Tables</Typography>
+              <ExpandMoreIcon />
+            </Box>
+          </Button>
+
+          {/* Contains the list of tables of the current catalogue */}
+          <Collapse in={isTablesOpen} unmountOnExit>
+            {items.length === 0 ? (
+              <EmptyState
+                title={DEFAULT_STRINGS.NO_TABLES_EXIST}
+                titleVariant="h6"
+                subtitle={DEFAULT_STRINGS.IMPORT_NEW_DATA_MESSAGE}
+              />
+            ) : (
+              <List>
+                {items.map((item, index) => (
+                  <SidebarListItem
+                    key={`${item.tableName}-${index}-table-metadata`}
+                    listItem={item}
+                  />
+                ))}
+              </List>
+            )}
+          </Collapse>
+          <Box py={2}>
+            <SidebarListItem
+              listItem={getSyntaxMockData()}
+              icon={<Book />}
+              subtitle={"Queries"}
+            />
+          </Box>
+        </Stack>
+      </Scrollbar>
     </Drawer>
   );
 };
